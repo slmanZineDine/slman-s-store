d8639c64f0d85866452f521ab76f122ede86565b
Slman <slman.zinedine1@gmail.com>
2022-01-12 14:50:51 +0200

    upload the project

---
+++ js/sub_ul.js
@@ -0,0 +1,8 @@
+// let theUl = document.getElementsByClassName("sub-ul"),
+//     theButton = document.querySelectorAll(".sections .practice .main-ul > li > a");
+
+
+// theButton[0].addEventListener("click", function(event) {
+//     event.preventDefault ();
+//     theUl[0].setAttribute("class","sub-ul active");
+// })
